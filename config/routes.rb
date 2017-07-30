71a75e43cd2c6b24a67a2c54decc1138f1c35a71
Roger Li <email": null>
2017-07-30 09:25:41 -0700

    Import routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-	root 'users#satori'
+	resources :users, only: [:index, :show]
+
+  get '/notify', to: 'users#notify_users'
+  get '/users/:id/status/:status', to: 'users#update_status'
+  get '/users/:id/location', to: 'users#update_location'
+	get '/users/:id/contacts', to: 'users#contacts'
 	get '/rapidio', to: 'users#rapid'
+
+	root 'users#satori'
+
 end
