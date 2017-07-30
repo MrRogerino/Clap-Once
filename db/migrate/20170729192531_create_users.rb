23518b1738a595b3701eb8537bbdf69a34be5b76
Roger Li <email": null>
2017-07-30 07:55:06 -0700

    Edit user migration to use longitude and latitude, as well as a status field

--- db/migrate/20170729192531_create_users.rb
+++ db/migrate/20170729192531_create_users.rb
@@ -3,9 +3,11 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     	create_table :users do |t|
     		t.text :first_name
     		t.text :last_name
-			t.text :phone    		
-    		t.text :location
-			t.integer :rapid_id
+				t.text :phone
+    		t.text :status, default: "safe"
+				t.decimal :latitude, precision: 8, scale: 5, default: 0
+	      t.decimal :longitude, precision: 8, scale: 5, default: 0
+				t.integer :rapid_id
 
 	  		t.timestamps
     	end
